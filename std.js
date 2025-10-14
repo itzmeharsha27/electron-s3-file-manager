5e17a0fb2f59642b13d852ff3c58944a8390c3b8
Itzmeharsha27 <reddyharshavardhan2705@gmail.com>
2025-10-14 20:29:05 +0530

    feat(user-roles): add admin/user roles and access middleware

--- std.js
+++ std.js
@@ -1,2 +1,8 @@
 const authRoutes = require('./routes/auth');
 app.use('/', authRoutes);
+
+
+const roleCheck = require('./middleware/roleCheck');
+app.get('/admin-dashboard', roleCheck('admin'), (req, res) => {
+  res.send('Admin dashboard');
+});
